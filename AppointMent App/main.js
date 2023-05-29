42fa54469d22528ca7843dc78eee8da687be6149
Ganeshbjamkar <you@example.com>
2023-05-29 09:33:36 +0530

    Delete data from Local Storage

--- AppointMent App/main.js
+++ AppointMent App/main.js
@@ -48,3 +48,65 @@ document.addEventListener('DOMContentLoaded', function() {
     // displayAppointments();
   });
   
+ ///// Delete appointment
+
+ // Retrieve appointments from local storage (if any)
+var appointments = JSON.parse(localStorage.getItem('appointments')) || [];
+
+// Function to render appointments on the web page
+function renderAppointments() {
+  var appointmentsList = document.getElementById('appointments-list');
+  appointmentsList.innerHTML = '';
+
+  appointments.forEach(function(appointment, index) {
+    var appointmentItem = document.createElement('li');
+    appointmentItem.textContent = appointment.name + ' - ' + appointment.date + ' - ' + appointment.time;
+
+    var deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.addEventListener('click', function() {
+      deleteAppointment(index);
+    });
+
+    appointmentItem.appendChild(deleteButton);
+    appointmentsList.appendChild(appointmentItem);
+  });
+}
+
+// Function to delete an appointment
+function deleteAppointment(index) {
+  appointments.splice(index, 1);
+  localStorage.setItem('appointments', JSON.stringify(appointments));
+  renderAppointments();
+}
+
+// Function to handle form submission
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  var nameInput = document.getElementById('name');
+  var dateInput = document.getElementById('date');
+  var timeInput = document.getElementById('time');
+
+  var appointment = {
+    name: nameInput.value,
+    date: dateInput.value,
+    time: timeInput.value
+  };
+
+  appointments.push(appointment);
+  localStorage.setItem('appointments', JSON.stringify(appointments));
+  renderAppointments();
+
+  nameInput.value = '';
+  dateInput.value = '';
+  timeInput.value = '';
+}
+
+// Add event listener to the form
+var appointmentForm = document.getElementById('appointment-form');
+appointmentForm.addEventListener('submit', handleFormSubmit);
+
+// Initial rendering of appointments
+renderAppointments();
+
